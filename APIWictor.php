906614190f35942252582ea2a55fcd57ba5f58f6
WictorSvensson <wictor110@hotmail.com>
2019-02-22 20:45:43 +0100

    function for search and top popularadded

--- APIWictor.php
+++ APIWictor.php
@@ -3,14 +3,49 @@
 include('./httpful.phar');
 
 $uri =  "https://api-v3.igdb.com/games";
-
 $key = '25adf5f6b515bd32214172f04eaeeb67';
 
-$response = \Httpful\Request::post($uri) 
+
+#echo ReadMostPopular($uri, $key);
+
+function ReadMostPopular($uri, $key){
+	$response = \Httpful\Request::post($uri) 
 	->addHeader('user-key', $key)
-	->body('limit 1; fields name ; search "tetris";')
+	->body('limit 50; fields names; popularity ; sort popularity desc;')
 	->send();
 
-echo $response;
+	return $response;
+}
+
+$searchWord = "Kingdom Hearts III";
+
+#echo SearchGame($uri, $key, $searchWord);
+
+function SearchGame($uri, $key, $searchWord){
+	$response = \Httpful\Request::post($uri) 
+	->addHeader('user-key', $key)
+	->body('limit 10; fields name; search "'.$searchWord. ' ";')
+	->send();
+
+	return $response;
+}
+
+$genre = 4;
+
+echo CategorizeGames($uri, $key, $genre);
+
+function CategorizeGames($uri, $key, $genre){
+	$response = \Httpful\Request::post($uri) 
+	->addHeader('user-key', $key)
+	->body('limit 10; fields name; where genres = '.$genre. ' ;')
+	->send();
+
+	return $response;
+}
+
+
 
 ?>
+
+
+

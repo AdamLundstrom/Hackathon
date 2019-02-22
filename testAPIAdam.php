f8da8f772f63347130e824bdb8c6383801f9aaf4
AdamLundstrom <adam.lundstrom@hotmail.com>
2019-02-22 22:35:26 +0100

    download top rating data

--- testAPIAdam.php
+++ testAPIAdam.php
@@ -1,34 +1,39 @@
 <?php
-//ini_set('max_execution_time', 3600);
-$xml=simplexml_load_file("Provresultat.xml");
-//include('httpful.phar');
-$array=$xml->R;
-$oldPlace="?";
-$weatherTemp = "";
-$i=1;
+
 include('httpful.phar');
-
-$response = \Httpful\Request::post($uri)	
-    ->sendsXml()
+//ranking
+//$uri =  "https://api.appmonsta.com/v1/stores/android/rankings/aggregate	.json?country=US&date=2019-02-21";
+$uri =  "https://api.appmonsta.com/v1/stores/android/rankings.json?country=US&date=2019-02-21";
+$user = "ba1a9efaa156d10ee4034bc8e4d7ff145649578a";
+//56f354edcfd878161746a0acf083049556550680
+$password = "X";
+//$response = "";
+$response = \Httpful\Request::get($uri)	
+    ->authenticateWith($user, $password)
     ->send();
+$response = ($response->body);
+//echo var_dump($response);
+$arrayOfId = explode("\n", $response);
+//print_r ($arrayOfId);
+//$response = null;
 
-$url = "http://opendata-download-metfcst.smhi.se/api/category/pmp2g/version/2/geotype/point/lon/$lng/lat/$lat/data.json";
-$response = \Httpful\Request::get($url)
-->send();
-$rows = json_decode($response, true);
-$timeNow=date("Y-m-d h:a",time());
-$rowsUsed = $rows['timeSeries'];
-if(count((array) $rowsUsed)!=0){		
-	foreach($rowsUsed as $time){				
-		$bT=strtotime($time['validTime']);
-		$t=date("Y-m-d h:a",strtotime("-2 hours",$bT));
-		if($t==$timeNow){
-			$rowsUsed=$time['parameters'];
-			foreach($rowsUsed as $weather){
-				if($weather['name']=='t'){
-					$weatherTemp=$weather['values'][0];
-				}
-				
+//echo $response
+//echo json_encode($response,true);
+//$array = json_decode($arrayOfId,true);
+$i2 = 3;
+//cho (var_dump(json_decode($arrayOfId[1],true)));
+//echo json_decode($response,true);
 
 
+for($i = 0; $i < 9; $i++){
+	
+		
+	$arrayOfId[$i] = json_decode($arrayOfId[$i],true);
+	echo $arrayOfId[$i]["app_name"]."<br>";
+	
+	
+	
+}
+
+	
 ?>					

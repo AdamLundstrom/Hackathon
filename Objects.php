146a480ccb9e61cb0b0708e2bb88c483c9f5bacc
Kod25 <kalle.dahlin@hotmail.com>
2019-02-22 21:17:37 +0100

    Fixa alla tables och add/get funktioner

--- Objects.php
+++ Objects.php
@@ -1,124 +1,86 @@
 <?php
 
-class Person{
+class User{
 	//konstruerar en användare 
-	private $personID;
-	private $password;
-	private $firstName;
-	private $lastName;
-	private $phoneNr;
-	private $longitude;
-	private $latitude;
-	function __construct($personID, $password, $firstName, $lastName, $phoneNr,$lat,$lng, $interest){
-		
-		$this->personID= $personID;
-		$this->password= $password;
-		$this->firstName= $firstName;
-		$this->lastName= $lastName;
-		$this->longitude = $lng;
-		$this->latitude = $lat;	
-		$this->interest= $interest;	
-		$this->phoneNr= $phoneNr;
+	private $userID;
+	private $userName;
+	private $userPassword;
+	private $email;
+
+	function __construct($userID, $userName, $userPassword, $email){
+		$this->userID = $userID;
+		$this->userName = $userName;
+		$this->userPassword = $userPassword;
+		$this->email = $email;
 	}
 	//hämtar uppgifter om användaren
-	function getPersonID(){
-		return $this->personID;
+	function getUserID(){
+		return $this->userID;
 	}
-	function getPassword(){
-		return $this->password;
+	function getuserName(){
+		return $this->userName;
 	}	
-	function setFirstName($firstName){
-		$this->firstName= $firstName;
+	function getuserPassword($userPassword){
+		return $this->userPassword;
 	}
-	function getFirstName(){
-		return $this->firstName;
+	function getEmail(){
+		return $this->email;
+	}
+	function setuserName(){
+		$this->userName= $userName;
 	}	
-	function setLastName($lastName){
-		$this->lastName= $lastName;
-	}
-	function getLastName(){
-		return $this->lastName;
-	}
-	function getLatitude(){
-		return $this->latitude;
-	}
-	function getLongitude(){
-		return $this->longitude;
-	}
-	function setInterest($interest){
-		$this->interest=$interest;
-	}
-	function getInterest(){
-		return $this->interest;
-	}	
-	function setPhoneNr($phoneNr){
-	$this->phoneNr=$phoneNr;
-	}
-	function getPhoneNr(){
-		return $this->phoneNr;
-	}
-	function __toString(){
-		return $this->firstName." ".$this->lastName." ".$this->phoneNr; 
+	function setuserPassword($userPassword){
+		$this->userPassword= $userPassword;
 	}
 }
-class Event{
-	private $eventID;
+
+class Post{
+	private $postID;
+	private $userID;
+	private $posted;
 	private $title;
-	private $longitude;
-	private $latitude;
-	private $description;
-	private $datee;	
-	private $timee;		
-	//konstruerar ett objekt med turer
-	function __construct($eventID, $title,$description,$lat,$lng,$datee,$timee){
-		$this->eventID= $eventID;
-		$this->title= $title;
-		$this->longitude = $lng;
-		$this->latitude = $lat;	
-		$this->description= $description;
-		$this->datee= $datee;	
-		$this->timee= $timee;		
+	private $postText;
+
+	function __construct($postID, $userID, $posted, $title, $postText){
+		$this->$postID = $postID;
+		$this->$userID = $userID;
+		$this->$posted = $posted;
+		$this->$title = $title;
+		$this->$postText = $postText;
 	}
-	//hämtar uppgifter om användaren
-	function getEventID(){
-		return $this->eventID;
+	function getPostID(){
+		return $this->postID;
 	}
-	function getLatitude(){
-		return $this->latitude;
+	function getUserID(){
+		return $this->userID;
 	}
-	function getLongitude(){
-		return $this->longitude;
-	}
-	
-	function setTitle($title){
-		$this->title= $title;
+	function getPosted(){
+		return $this->posted;
 	}
 	function getTitle(){
 		return $this->title;
-	}	
-	function setAddress($address){
-		$this->address= $address;
 	}
-	function getAddress(){
-		return $this->address;
-	}
-	function setDescription($description){
-		$this->description= $description;
-	}
-	function getDescription(){
-		return $this->description;
-	}
-	function setDatee($datee){
-		$this->datee=$datee;
-	}
-	function getDatee(){
-		return $this->datee;
-	}	
-	function setTimee($timee){
-		$this->timee=$timee;
-	}
-	function getTimee(){
-		return $this->timee;
+	function getPostText(){
+		return $this->postText;
 	}
 }
+
+class Thread{
+	private $gameid;
+	private $threadName;
+
+	function __construct($gameid, $threadName){
+		$this->gameid = $gameid;
+		$this->threadName = $threadName;
+	}
+
+	function getGameID(){
+		return $this->gameID;
+	}
+
+	function getThreadName(){
+		return $this->threadName;
+	}
+}
+
 ?>
--- db_connection.php
+++ db_connection.php
@@ -1,17 +1,6 @@
 <?php
 include 'Objects.php';
-class db_connection{	
-	private $personList=array();
-	private $eventList=array();
-	private $userWithBookingList=array();
-	private $pNumber="";
-	private $fName=""; 
-	private $lName="";
-	private $country="";
-	private $username="";
-	private $password="";		
-	private $bookingDate=""; 
-
+class db_connection{
 	//konstruktorn som skapar en array med alla användare och turer 
 	function __construct(){
 		//databaskopplingens uppgifter
@@ -31,7 +20,7 @@ class db_connection{
 				die("Connection failed: " . $this->connection->connect_error);
 			} 
 			//skapa en ny databas
-			$sql = "CREATE DATABASE hackathon";
+			$sql = "CREATE DATABASE IF NOT EXISTS hackathon";
 			
 			if ($this->connection->query($sql) === FALSE) {
 				echo "Error creating database: " . $this->connection->error;
@@ -42,28 +31,42 @@ class db_connection{
 		}
 
 		//om tabellen inte existerar skapas en ny
-		$sql= "CREATE TABLE IF NOT EXISTS User(
-		userID int AUTO_INCREMENT PRIMARY KEY,
-		userName varchar(30) NOT NULL,
-		userPassword varchar(30) NOT NULL
-		)";	
+		$sql = "CREATE TABLE IF NOT EXISTS User(
+			userID int AUTO_INCREMENT PRIMARY KEY,
+			userName varchar(30) NOT NULL,
+			userPassword varchar(30) NOT NULL,
+			email varchar(30) NOT NULL,
+			UNIQUE (userName)
+			)";	
 		if ($this->connection->query($sql) === FALSE) { 
 			echo "Error creating table: 1	" , $this->connection->error, "<br>";
 		} 
 
-		//om tabellen inte existerar skapas en ny
-		$sql= "CREATE TABLE IF NOT EXISTS ForumPost(
-		forumPostID int AUTO_INCREMENT PRIMARY KEY,
-		userID int NOT NULL,
-		FOREIGN KEY (userID) REFERENCES User(userID),
-		posted DATETIME NOT NULL,
-		title varchar(50) NOT NULL,
-		postText varchar(250) NOT NULL
-		)";
+		$sql = "CREATE TABLE IF NOT EXISTS ForumThread(
+			gameID int PRIMARY KEY,
+			threadName varchar(30) NOT NULL,
+			UNIQUE (gameID),
+			UNIQUE (threadName)
+			)";	
+
+		if ($this->connection->query($sql) === FALSE) { 
+				echo "Error creating table: 2	" , $this->connection->error, "<br>";
+		} 
+
+		
+		$sql = "CREATE TABLE IF NOT EXISTS ForumPost(
+			forumPostID int AUTO_INCREMENT PRIMARY KEY,
+			gameID int NOT NULL,
+			FOREIGN KEY (gameID) REFERENCES ForumThread(gameID),
+			userID int NOT NULL,
+			FOREIGN KEY (userID) REFERENCES User(userID),
+			posted DATETIME NOT NULL,
+			title varchar(50) NOT NULL,
+			postText varchar(250) NOT NULL	
+			)";
 		
 		if ($this->connection->query($sql) === FALSE) { 
-			echo "Error creating table: 2	" , $this->connection->error, "<br>";
-			$this->dropDatabase();
+			echo "Error creating table: 3	" , $this->connection->error, "<br>";
 		}
 		
 	}	
@@ -75,38 +78,99 @@ class db_connection{
 		}
 	}
 
-	function addApp(){
-		$sql = "";
-		if ($this->connection->query($sql) === False) {
-			echo "Error: " . $sql . "<br>" . $this->connection->error;
-		}
-	}
-
-	function addUser($newUserName, $newUserPW){
-		$sql = "INSERT INTO User (userName, userPassword)
-		Values ('$newUserName', '$newUserPW')";
+	function addUser($newUserName, $newUserPW, $email){
+		$sql = "INSERT INTO User (userName, userPassword, email)
+		Values ('$newUserName', '$newUserPW', '$email')";
 
 		if ($this->connection->query($sql) === False) {
 			echo "Error: " . $sql . "<br>" . $this->connection->error;
 		}
 	}
 
-	function addForumPost($userID, $title, $postText){
+	
+	function getUser($username){
+		$sql="SELECT * FROM User WHERE userName='$username'";
+		$result=$this->connection->query($sql);
+		if($result->num_rows > 0){	
+			$row=$result->fetch_assoc();
+			$user = new User($row["userID"],$row['userName'],$row["userPassword"], $row["email"]);
+			return $user;	
+		}
+	}
+
+	function addForumPost($gameID, $userID, $title, $postText){
 		$posted = date("Y-m-d H:i:s");
-		$sql = "INSERT INTO ForumPost (userID, posted, title, postText)
-		Values ('$userID', '$posted', '$title', '$postText')";
+		$sql = "INSERT INTO ForumPost (gameID, userID, posted, title, postText)
+		Values ('$gameID', '$userID', '$posted', '$title', '$postText')";
 
 		if ($this->connection->query($sql) === False) {
 			echo "Error: " . $sql . "<br>" . $this->connection->error;
 		}
-	}	
+	}
+
+	function getAllForumPost(){
+		$sql="SELECT * FROM ForumPost";
+		$result=$this->connection->query($sql);
+		$postList = array();
+		if($result->num_rows > 0){	
+			while($row=$result->fetch_assoc()){	
+				$postList[] = new Post($row["forumPostID"], $row['gameID'], $row['userID'],$row["posted"],$row["title"],$row["postText"]);
+			}	
+		}
+		return $postList;
+	}
+
+	function addForumThread($gameID, $threadName){
+		$sql = "INSERT INTO ForumThread (gameID, threadName)
+		Values ('$gameID', '$threadName')";
+
+		if ($this->connection->query($sql) === False) {
+			echo "Error: " . $sql . "<br>" . $this->connection->error;
+		}
+	}
+
+	function getAllForumThread(){
+		$sql="SELECT * FROM ForumThread";
+		$result=$this->connection->query($sql);
+		$threadList = array();
+		if($result->num_rows > 0){	
+			while($row=$result->fetch_assoc()){	
+				$threadList[] = new Thread($row["gameID"], $row['threadName']);
+			}	
+		}
+		return $threadList;
+	}
+
 }
 
 $db = new db_connection;
 
-$db->addUser("Alice", "AAAAA");
-$db->addUser("Bob", "BBBBB");
-$db->addForumPost(1, "Title", "Text");
+/*
+$db->addUser("Alice", "AAAAA", "email@email");
+$db->addUser("Bob", "BBBBB", "email@email");
+$db->addUser("Code", "CCCCC", "email@email");
+*/
+
+#$db->addForumThread(44, "DOOOOOM");
+#$db->addForumThread(2, "Tetris");
+
+/*
+$db->addForumPost(44, 1, "Title1", "Textttttttttttttttt");
+$db->addForumPost(44, 1, "Title2", "Text");
+$db->addForumPost(44, 2, "Title3", "Text");
+$db->addForumPost(44, 2, "Title4", "Textttttttttttttttt");
+*/
+
+#$user = $db->getUser("Bob");
+#echo $user->getUserName();
+
+#$posts = $db->getAllForumPost();
+#echo count($posts);
+
+#$threads = $db->getAllForumThread();
+#echo count($threads);
+
+#$posts['']->getPostText();
 
 #$db->dropDatabase();
 
